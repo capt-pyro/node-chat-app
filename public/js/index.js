bb46d859464aa8bb35f099aac32bc8f2cda08f94
capt-pyro <joe.nsj@gmail.com>
2017-06-28 17:21:29 +0400

    improved user interface added stylesheet and animative responses

--- public/js/index.js
+++ public/js/index.js
@@ -17,36 +17,42 @@ socket.on('newMessage', function (message) {
 });
 socket.on('newLocationMessage', function (message) {
   var li = jQuery('<li></li>');
-  var a = jQuery('<a target="_blank">My current Location</a>');//target blank opens new tab
+  var a = jQuery('<a target="_blank">My Current Location</a>');//target blank opens new tab
   li.text(`${message.from}: `);
   a.attr('href',message.url);
   li.append(a);
   jQuery('#messages').append(li);
 });
 
-
+var messageBox = jQuery('[name=message]');
 jQuery('#message-form').on('submit', function(e) {
   e.preventDefault();
-
   socket.emit('createMessage', {
     from: 'User',
-    text: jQuery('[name=message]').val()
-  }, function (returnConfirm) {
-    console.log(returnConfirm);
+    text: messageBox.val()
+  }, function () {
+    messageBox.val('')
   });
 });
 
 var locationButton = jQuery('#send-location');
-locationButton.on('click', function () {
-  if(!navigator.geolocation) return alert('geolocation not supported by your browser!!');
-  navigator.geolocation.getCurrentPosition(function (position) {
-    socket.emit('createLocationMessage', {
-      latitude: position.coords.latitude,
-      longitude: position.coords.longitude
-    },function (returnConfirm) {
-      console.log(returnConfirm);
+  locationButton.on('click', function () {
+    if(!navigator.geolocation) {
+      return alert('geolocation not supported by your browser!!')
+    };
+    locationButton.attr('disabled','disabled').text('Sending location ...');
+    navigator.geolocation.getCurrentPosition(function (position) {
+      socket.emit('createLocationMessage', {
+        latitude: position.coords.latitude,
+        longitude: position.coords.longitude
+      },function (returnConfirm) {//acknowledgement from server
+        console.log(returnConfirm);
+      });
+      locationButton.removeAttr('disabled').text('Send location');
+    },function () {
+      locationButton.attr('disabled','disabled').text('Location not found');
+      alert('Unable to fetch current location. Please turn on location');
+      locationButton.removeAttr('disabled').text('Send location');
     });
-  },function () {
-    alert('Unable to fetch current location. Please turn on location');
-  });
+
 });
